1d44bc8b59e7b847cfc4fb6d2c79830e3db89b23
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-07 05:01:08 +0300

    v0.1.0

---
+++ src/App.tsx
@@ -0,0 +1,342 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-800">
+      <nav className="bg-neutral-900 rounded-lg fixed h-8 top-2 left-4 right-4 px-1">
+        {/* left Side  */}
+        <div className="h-full flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="#0a0a0a"
+            className="w-6 h-6 bg-neutral-300 px-1 rounded-lg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </div>
+      </nav>
+      <main className="w-full min-h-screen pt-12 max-w-7xl mx-auto pl-64 pr-64 selection:bg-neutral-600 selection:text-neutral-200">
+        <article className="bg-neutral-200 w-full min-h-96 rounded-lg p-6">
+          {/* Section Banner  */}
+          <div className="mb-4">
+            <img
+              className="w-full rounded-lg"
+              src="https://fakeimg.pl/800x200"
+              alt=""
+            />
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
+            amet eum autem quae. Fuga sequi sint corporis. Quasi ad iste
+            consequuntur quo animi necessitatibus earum porro illo cumque, id,
+            velit, error cum. In quod corrupti deleniti minima voluptatum
+            consequatur minus vitae recusandae porro blanditiis exercitationem
+            et hic saepe ab, provident laboriosam, similique adipisci alias,
+            quis repellat! Natus obcaecati totam quo nobis error earum,
+            provident, eveniet quaerat corporis culpa aliquam, numquam quas
+            minus voluptas voluptatem ut harum beatae quam facere illum magni
+            voluptatum. Expedita iusto quae distinctio assumenda dolor deserunt
+            rerum laboriosam nesciunt consequatur quis? Recusandae impedit
+            delectus, quod repellat quibusdam aut inventore veritatis. Aliquid
+            dicta, ab consequatur molestias sunt, dolor magnam quaerat inventore
+            maiores voluptatibus voluptatum quasi quos illum officia cupiditate
+            accusantium, possimus numquam sequi! Harum dolores rerum natus, est
+            nulla dignissimos non ab sed nemo vero at, doloribus repudiandae
+            reiciendis? Aut sed quo sunt asperiores aliquam nesciunt, qui
+            sapiente voluptatum optio corporis, minima ratione. Molestias
+            asperiores voluptates est minima harum, ipsum, in eaque ratione
+            necessitatibus dolorum quisquam alias vero quo pariatur ab!
+            Voluptatem harum laudantium, molestiae odio aliquid voluptatum!
+            Quod, rem nihil odit impedit vitae asperiores omnis doloremque
+            placeat error optio fugit vero architecto distinctio quibusdam?
+            Accusantium alias blanditiis odio ab quidem tempore expedita
+            mollitia! Excepturi enim quisquam fugiat impedit odit, possimus
+            eligendi, similique cupiditate dolore ratione id tenetur at? Aperiam
+            consequatur repudiandae est delectus, ut commodi possimus architecto
+            ipsam suscipit nisi repellendus voluptatibus tempore voluptatum
+            doloremque, autem molestiae obcaecati non expedita explicabo eaque
+            impedit vitae unde aliquam laudantium. Molestiae, minus? Quos
+            officiis adipisci, voluptatem vero omnis provident iure earum
+            repellat iste nostrum blanditiis inventore est debitis labore
+            laborum rem quas. Quasi, vitae perspiciatis facere nam illum, dicta
+            quos ab modi, impedit quisquam alias itaque fugit delectus!
+            Temporibus adipisci ipsam illo quas, mollitia deleniti praesentium
+            aut illum nulla in!
+          </p>
+          <div className="px-8">
+            <img
+              className="mx-auto my-6 rounded-lg"
+              src="https://fakeimg.pl/800x600"
+              alt=""
+            />
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
+            amet eum autem quae. Fuga sequi sint corporis. Quasi ad iste
+            consequuntur quo animi necessitatibus earum porro illo cumque, id,
+            velit, error cum. In quod corrupti deleniti minima voluptatum
+            consequatur minus vitae recusandae porro blanditiis exercitationem
+            Accusantium alias blanditiis odio ab quidem tempore expedita
+            mollitia! Excepturi enim quisquam fugiat impedit odit, possimus
+            eligendi, similique cupiditate dolore ratione id tenetur at? Aperiam
+            consequatur repudiandae est delectus, ut commodi possimus architecto
+            ipsam suscipit nisi repellendus voluptatibus tempore voluptatum
+            doloremque, autem molestiae obcaecati non expedita explicabo eaque
+            impedit vitae unde aliquam laudantium. Molestiae, minus? Quos
+            officiis adipisci, voluptatem vero omnis provident iure earum
+            repellat iste nostrum blanditiis inventore est debitis labore
+            laborum rem quas. Quasi, vitae perspiciatis facere nam illum, dicta
+            quos ab modi, impedit quisquam alias itaque fugit delectus!
+            Temporibus adipisci ipsam illo quas, mollitia deleniti praesentium
+            aut illum nulla in!
+          </p>
+          <div className="px-8 w-fit grid grid-cols-2 gap-2 my-6 mx-auto">
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
+            amet eum autem quae. Fuga sequi sint corporis. Quasi ad iste
+            consequuntur quo animi necessitatibus earum porro illo cumque, id,
+            velit, error cum. In quod corrupti deleniti minima voluptatum
+            consequatur minus vitae recusandae porro blanditiis exercitationem
+            et hic saepe ab, provident laboriosam, similique adipisci alias,
+            quis repellat! Natus obcaecati totam quo nobis error earum,
+            provident, eveniet quaerat corporis culpa aliquam, numquam quas
+            minus voluptas voluptatem ut harum beatae quam facere illum magni
+            voluptatum. Expedita iusto quae distinctio assumenda dolor deserunt
+            rerum laboriosam nesciunt consequatur quis? Recusandae impedit
+            delectus, quod repellat quibusdam aut inventore veritatis. Aliquid
+            dicta, ab consequatur molestias sunt, dolor magnam quaerat inventore
+            maiores voluptatibus voluptatum quasi quos illum officia cupiditate
+            accusantium, possimus numquam sequi! Harum dolores rerum natus, est
+            nulla dignissimos non ab sed nemo vero at, doloribus repudiandae
+            reiciendis? Aut sed quo sunt asperiores aliquam nesciunt, qui
+            sapiente voluptatum optio corporis, minima ratione. Molestias
+            asperiores voluptates est minima harum, ipsum, in eaque ratione
+            necessitatibus dolorum quisquam alias vero quo pariatur ab!
+            Voluptatem harum laudantium, molestiae odio aliquid voluptatum!
+            Quod, rem nihil odit impedit vitae asperiores omnis doloremque
+            placeat error optio fugit vero architecto distinctio quibusdam?
+            Accusantium alias blanditiis odio ab quidem tempore expedita
+            mollitia! Excepturi enim quisquam fugiat impedit odit, possimus
+            eligendi, similique cupiditate dolore ratione id tenetur at? Aperiam
+            consequatur repudiandae est delectus, ut commodi possimus architecto
+            ipsam suscipit nisi repellendus voluptatibus tempore voluptatum
+            doloremque, autem molestiae obcaecati non expedita explicabo eaque
+            impedit vitae unde aliquam laudantium. Molestiae, minus? Quos
+            officiis adipisci, voluptatem vero omnis provident iure earum
+            repellat iste nostrum blanditiis inventore est debitis labore
+            laborum rem quas. Quasi, vitae perspiciatis facere nam illum, dicta
+            quos ab modi, impedit quisquam alias itaque fugit delectus!
+            Temporibus adipisci ipsam illo quas, mollitia deleniti praesentium
+            aut illum nulla in!
+          </p>
+
+          <div className="px-8 grid grid-cols-2 w-fit  gap-2 my-6 mx-auto">
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa magni
+            pariatur odio nam? Incidunt sequi consequatur, nesciunt totam ut,
+            dignissimos accusamus nulla, saepe doloribus possimus distinctio?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+            quia, sint at tenetur rerum rem labore architecto quibusdam iure
+            fuga quis, incidunt odit quas autem cum eos. Soluta corporis, autem
+            explicabo aut doloribus ex praesentium quibusdam, accusamus deserunt
+            impedit magni?
+          </p>
+          <div className="px-8 flex w-fit flex-wrap justify-center gap-2 my-6 mx-auto">
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+            <img
+              className="rounded-lg"
+              src="https://fakeimg.pl/300x300"
+              alt=""
+            />
+          </div>
+        </article>
+      </main>
+      {/* sideBar Content [Start] */}
+      <div className=" bg-neutral-700 h-[calc(100%-4rem)] w-56 fixed right-4 top-12 bottom-4 rounded-lg"></div>
+      {/* sideBar Content [End] */}
+
+      {/* sideBar Section [Start] */}
+      <div className="bg-neutral-950 pr-1.5 h-[calc(100%-4rem)] fixed overflow-hidden left-4 top-12 bottom-4 rounded-lg">
+        <aside className="h-full  w-56 p-2 overflow-y-auto">
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit amet.
+          </p>
+          <p className="text-neutral-50 font-semibold my-2 ">
+            Lorem ipsum dolor sit.
+          </p>
+        </aside>
+      </div>
+      {/* sideBar Section [End] */}
+    </div>
+  );
+}
+
+export default App;
