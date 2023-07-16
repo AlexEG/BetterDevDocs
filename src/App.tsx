f798c68834fad88ab055a018a0a28ad859a91889
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-16 14:22:35 +0300

    v1.4.0
    START FROM 0
    again

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import DocsHomePage from "./pages/DocsHomePage";
-import Page404 from "./pages/Page404";
-
-import JSMDNRoutes from "./pages/JS/JSMDNRoutes";
-
-import JSHome from "./pages/JS/JSHome";
-import MainLayout from "./pages/MainLayout";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DocsHomePage />}></Route>
-        <Route path="/test" element={<MainLayout />}></Route>
-
-        <Route path="/javascript" element={<JSHome />}></Route>
-        <Route path="/javascript/mdn/*" element={<JSMDNRoutes />}></Route>
-
-        <Route path="*" element={<Page404 />}></Route>
-      </Routes>
+      <Routes></Routes>
     </BrowserRouter>
   );
 }
