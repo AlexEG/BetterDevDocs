4fc7e3bc090718f4bf22845121068a065175440d
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-16 03:06:07 +0300

    1.3.3
    sidebar [works]

--- src/components/Sidebar/SidebarDocsSourceContainer.tsx
+++ src/components/Sidebar/SidebarDocsSourceContainer.tsx
@@ -6,6 +6,7 @@ interface SidebarDocsSourceContainer {
   chooseDocsSourceContainer(DocsSourceName: string): void;
   whatSubSectionIsOpen: string;
   ChangeSubSectionIsOpen(subSectionFullTitle: string): void;
+  sectionsTitleAndSubsections: object;
 }
 function SidebarDocsSourceContainer({
   DocsSourceName,
@@ -13,11 +14,27 @@ function SidebarDocsSourceContainer({
   chooseDocsSourceContainer,
   whatSubSectionIsOpen,
   ChangeSubSectionIsOpen,
+  sectionsTitleAndSubsections,
 }: SidebarDocsSourceContainer) {
   // Styles for the opened source tap
   const isDocsSourceContainerChosen_CSS = isDocsSourceContainerChosen
     ? "bg-neutral-800 text-center"
     : "bg-neutral-900";
+
+  // All Sidebar Docs Source Section Components
+  let components = [];
+  for (const key in sectionsTitleAndSubsections) {
+    // console.log(key);
+    // console.log(sectionsTitleAndSubsections[key]);
+    components.push(
+      <SidebarDocsSourceSection
+        sectionTitle={key}
+        subSectionsArr={sectionsTitleAndSubsections[key]}
+        whatSubSectionIsOpen={whatSubSectionIsOpen}
+        ChangeSubSectionIsOpen={(e) => ChangeSubSectionIsOpen(e)}
+      />,
+    );
+  }
 
   return (
     <section
@@ -29,59 +46,7 @@ function SidebarDocsSourceContainer({
       </h2>
 
       <div className="pl-4 pr-2">
-        {isDocsSourceContainerChosen && (
-          <>
-            <SidebarDocsSourceSection
-              sectionTitle="JavaScript first steps"
-              subSectionsArr={[
-                "JavaScript First Steps",
-                "What is JavaScript?",
-                "A first splash into JavaScript",
-                "What went wrong? Troubleshooting JavaScript",
-                "Storing the information you need Variables",
-                "Basic math in JavaScript numbers and operators",
-                "Handling text strings in JavaScript",
-                "Useful string methods",
-                "Arrays",
-                "Silly story generator",
-              ]}
-              whatSubSectionIsOpen={whatSubSectionIsOpen}
-              ChangeSubSectionIsOpen={(e) => ChangeSubSectionIsOpen(e)}
-            />
-
-            <SidebarDocsSourceSection
-              sectionTitle="JavaScript building blocks"
-              subSectionsArr={[
-                "JavaScript building blocks",
-                "Making decisions in your code conditionals",
-                "Looping code",
-                "Functions reusable blocks of code",
-                "Build your own function",
-                "Function return values",
-                "Introduction to events",
-                "Image gallery",
-              ]}
-              whatSubSectionIsOpen={whatSubSectionIsOpen}
-              ChangeSubSectionIsOpen={(e) => ChangeSubSectionIsOpen(e)}
-            />
-
-            <SidebarDocsSourceSection
-              sectionTitle="Introducing JavaScript objects"
-              subSectionsArr={[
-                "Introducing JavaScript objects",
-                "JavaScript object basics",
-                "Object prototypes",
-                "Object-oriented programming",
-                "Classes in JavaScript",
-                "Working with JSON",
-                "Object building practice",
-                "Adding features to our bouncing balls demo",
-              ]}
-              whatSubSectionIsOpen={whatSubSectionIsOpen}
-              ChangeSubSectionIsOpen={(e) => ChangeSubSectionIsOpen(e)}
-            />
-          </>
-        )}
+        {isDocsSourceContainerChosen && components}
       </div>
     </section>
   );
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -39,6 +39,63 @@ function Sidebar() {
   }
   // <SidebarDocsSourceSection />[End]
 
+  // DATA [Start]
+  const dataMDN = {
+    "JavaScript first steps": [
+      "JavaScript First Steps",
+      "What is JavaScript?",
+      "A first splash into JavaScript",
+      "What went wrong? Troubleshooting JavaScript",
+      "Storing the information you need Variables",
+      "Basic math in JavaScript numbers and operators",
+      "Handling text strings in JavaScript",
+      "Useful string methods",
+      "Arrays",
+      "Silly story generator",
+    ],
+    "JavaScript building blocks": [
+      "JavaScript building blocks",
+      "Making decisions in your code conditionals",
+      "Looping code",
+      "Functions reusable blocks of code",
+      "Build your own function",
+      "Function return values",
+      "Introduction to events",
+      "Image gallery",
+    ],
+    "Introducing JavaScript objects": [
+      "Introducing JavaScript objects",
+      "JavaScript object basics",
+      "Object prototypes",
+      "Object-oriented programming",
+      "Classes in JavaScript",
+      "Working with JSON",
+      "Object building practice",
+      "Adding features to our bouncing balls demo",
+    ],
+  };
+
+  const dataW3School = {
+    "JS Tutorial": [
+      "JS Home",
+      "JavaScript Introduction",
+      "JavaScript Where To",
+      "JavaScript Output",
+      "JavaScript Statements",
+      "JavaScript Syntax",
+      "JavaScript Comments",
+      "...",
+    ],
+    "JS Versions": [
+      "JavaScript ES5",
+      "Javascript ES6",
+      "ECMAScript 2016",
+      "...",
+      "ECMAScript 2021",
+    ],
+  };
+  // DATA [End]
+
   return (
     <aside
       className={`bg-neutral-950 h-[calc(100%-2.5rem)] fixed top-10 left-0 after:w-0 after:h-0 after:absolute after:top-0 after:right-0 after:border-[1.25rem] after:border-r-transparent after:border-b-transparent after:translate-x-full after:border-neutral-950 pt-10 ${isTheSidebarOpen_CSS}`}
@@ -62,6 +119,7 @@ function Sidebar() {
                 SidebarState.DocsSourceContainerChosen_SubSection
               }
               ChangeSubSectionIsOpen={(e) => whatSubSectionIsOpenFunc(e)}
+              sectionsTitleAndSubsections={dataMDN}
             />
 
             <SidebarDocsSourceContainer
@@ -74,30 +132,8 @@ function Sidebar() {
                 SidebarState.DocsSourceContainerChosen_SubSection
               }
               ChangeSubSectionIsOpen={(e) => whatSubSectionIsOpenFunc(e)}
+              sectionsTitleAndSubsections={dataW3School}
             />
-            {/* <SidebarDocsSourceContainer
-            DocsSourceName="JavaScript.info"
-            isDocsSourceContainerChosen={
-              SidebarState.DocsSourceContainerChosen === "JavaScript.info"
-            }
-            chooseDocsSourceContainer={(e) => chooseDocsSourceContainer(e)}
-            whatSubSectionIsOpen={
-              SidebarState.DocsSourceContainerChosen_SubSection
-            }
-            ChangeSubSectionIsOpen={(e) => whatSubSectionIsOpenFunc(e)}
-          />
-
-          <SidebarDocsSourceContainer
-            DocsSourceName="JDKJS Up & Going"
-            isDocsSourceContainerChosen={
-              SidebarState.DocsSourceContainerChosen === "JDKJS Up & Going"
-            }
-            chooseDocsSourceContainer={(e) => chooseDocsSourceContainer(e)}
-            whatSubSectionIsOpen={
-              SidebarState.DocsSourceContainerChosen_SubSection
-            }
-            ChangeSubSectionIsOpen={(e) => whatSubSectionIsOpenFunc(e)}
-          />  */}
           </div>
 
           <SidebarMenu />
