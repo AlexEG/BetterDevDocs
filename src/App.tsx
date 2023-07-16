ff9d2ac6005910350bb59b84860bba550f00a132
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-16 14:07:11 +0300

    v1.3.10
    DELETE a lot of code and start form 50%

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Page404 from "./pages/Page404";
 import JSMDNRoutes from "./pages/JS/JSMDNRoutes";
 
 import JSHome from "./pages/JS/JSHome";
+import MainLayout from "./pages/MainLayout";
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<DocsHomePage />}></Route>
+        <Route path="/test" element={<MainLayout />}></Route>
+
         <Route path="/javascript" element={<JSHome />}></Route>
         <Route path="/javascript/mdn/*" element={<JSMDNRoutes />}></Route>
 
--- src/DATA/SectionComingSoon.tsx
+++
@@ -1,7 +0,0 @@
-export default function SectionComingSoon() {
-  return (
-    <>
-      <h1>Section Coming Soon</h1>
-    </>
-  );
-}
