dc08edb7dde65091b43ee24da03ddc4107e61bec
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-20 06:50:25 +0300

    v1.6.3
    Discord btn

--- src/components/Sidebar/SidebarDocsSourceSection.tsx
+++ src/components/Sidebar/SidebarDocsSourceSection.tsx
@@ -25,13 +25,12 @@ function SidebarDocsSourceSection({
         to={`${DocsSourceName.toLocaleLowerCase()
           .split(" ")
           .join("-")}/${subSection.toLocaleLowerCase().split(" ").join("-")}`}
+        className="pt-1 pb-1 last:pb-0 first:pt-0"
+        onClick={() => ChangeSubSectionIsOpen(`${sectionTitle}-${subSection}`)}
       >
         <p
           key={`${sectionTitle}-${subSection}`}
-          onClick={() =>
-            ChangeSubSectionIsOpen(`${sectionTitle}-${subSection}`)
-          }
-          className={`before:absolute relative before:top-0 before:-left-3 pl-1 before:h-full before:w-0.5 my-1 cursor-pointer ${subSectionOpen_CSS}`}
+          className={`before:absolute relative before:top-0 before:-left-3 pl-1 before:h-full before:w-0.5  cursor-pointer ${subSectionOpen_CSS} `}
         >
           {subSection}
         </p>
@@ -44,7 +43,7 @@ function SidebarDocsSourceSection({
     <section className="flex-col flex mb-4 first-of-type:mt-2 ">
       <h2 className="text-neutral-50 font-semibold mb-0.5">{sectionTitle}</h2>
 
-      <div className="pl-3 flex flex-col text-neutral-300 select-none text-sm font-normal before:absolute relative before:top-0 before:left-0  before:h-full before:w-0.5 before:bg-neutral-600">
+      <div className="pl-3 flex flex-col text-neutral-300 select-none text-sm font-normal before:absolute relative before:top-0 before:left-0  before:h-full before:w-0.5 before:bg-neutral-600 ">
         {allSubSections}
       </div>
     </section>
--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,10 +1,12 @@
 import GitHubBtn from "./GitHubBtn";
-
+import Discord from "./Discord";
 function SidebarMenu() {
   return (
     <div className="h-10 bg-neutral-900 absolute bottom-0 left-0 w-full flex justify-around items-center">
       <GitHubBtn />
-      <button>
+      <Discord />
+
+      {/* <button>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -18,7 +20,8 @@ function SidebarMenu() {
             d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
           />
         </svg>
-      </button>
+      </button> */}
+
       <button>
         <svg
           xmlns="http://www.w3.org/2000/svg"
